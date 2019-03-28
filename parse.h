152c6c612f1378c2599e8a73c5afb41e9f31240d
Timur <hts1238@mail.ru>
2019-03-28 22:43:39 +0500

    Version v3.0

--- parse.h
+++ parse.h
@@ -10,7 +10,8 @@ using namespace std;
 
 double parse(const string& _s, const bool& _DEBUG_LOG);
 
-namespace Parse {
+class Parse {
+private:
     int i = 0;
     string s = "";
 
@@ -22,18 +23,17 @@ namespace Parse {
         Stack* next;
     };
 
+    Stack* res = NULL;
+    Stack* ops = NULL;
+
     /// Make and show debug message showing status of each global Stack ('res' and 'ops')
-    void debug(Stack*& top1) {
-        //Stack* top1 = res;
-        //Stack* top2 = ops;
-        cout << "\n [ Stack ] : ";
-        while (top1 != NULL) {
-        	cout << top1->field;
-        	cout << " ( " << (char)top1->field << " ) ";
-        	top1 = top1->next;
-        }
-        //cout << "\n [ ops ] : ";
-        //while (top2 != NULL) {cout << (char)top2->field << " "; top2 = top2->next;}
+    void debug() {
+        Stack* top1 = res;
+        Stack* top2 = ops;
+        cout << "\n [ res ] : ";
+        while (top1 != NULL) {cout << top1->field << " "; top1 = top1->next;}
+        cout << "\n [ ops ] : ";
+        while (top2 != NULL) {cout << (char)top2->field << " "; top2 = top2->next;}
         cout << "\n";
     }
 
@@ -48,14 +48,14 @@ namespace Parse {
         (*pointer).field = elem;
         (*pointer).next = top;
         top = pointer;
-        if (DEBUG_LOG) debug(top);
+        if (DEBUG_LOG) debug();
     }
 
     /// Pop from Stack 'top'
     double pop(Stack*& top) {
         Stack* pointer = top;
         top = (*top).next;
-        if (DEBUG_LOG) debug(top);
+        if (DEBUG_LOG) debug();
         return (*pointer).field;
     }
 
@@ -76,17 +76,17 @@ namespace Parse {
 
     /// Check if char 'ch' is number
     bool isNum(const char& ch) {
-        return '0' <= ch && ch <= '9';
+        return 0 <= toNum(ch) && toNum(ch) <= 9;
     }
 
     /// Make lowercase from char 'ch'
     char toLow(const char& ch) {
-    	return ('a' <= ch && ch <= 'z') ? ch : ch - 'A' + 'a';
+        return ('a' <= ch && ch <= 'z') ? ch : ch - 'A' + 'a';
     }
 
     /// Check if char 'ch' is letter
     bool isLet(const char& ch) {
-    	return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z');
+        return ('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z');
     }
 
     /// Get priority of the operator 'ch'
@@ -105,7 +105,7 @@ namespace Parse {
     }
 
     /// Calculate two latest numbers from 'res' using operator 'op'
-    double doit (const char& op, Stack*& res, Stack*& ops) {
+    double doit (const char& op) {
         if (DEBUG_LOG) cout << " " << op << "\n";
         double a, b;
 
@@ -129,11 +129,11 @@ namespace Parse {
         if (op == '-') return a - b;
         if (op == '*') return a * b;
         if (op == '/') {
-        	if (b == 0) {
-        		cout << "Time error! Division by zero!";
-        		_break_();
-        	}
-        	return a / b;
+            if (b == 0) {
+                cout << "Time error! Division by zero!";
+                _break_();
+            }
+            return a / b;
         }
 
         if (op == '^') return pow(a, b);
@@ -143,24 +143,24 @@ namespace Parse {
 
     /// Calculate number 'a' using function 'command'
     double doCommand(const string& command, const double& a) {
-    	if (command == "sqrt") return sqrt(a);
-    	if (command == "sin") return sin(a);
-    	if (command == "cos") return cos(a);
-    	//if (command == "abs") return abs(a);
-    	if (command == "exp") return exp(a);
-    	if (command == "cbrt") return cbrt(a);
-    	if (command == "tan") return tan(a);
-    	if (command == "log") return log(a);
-    	if (command == "log2") return log2(a);
-    	if (command == "log10") return log10(a);
-    	if (command == "arcsin") return asin(a);
-    	if (command == "arccos") return acos(a);
-    	if (command == "arctan") return atan(a);
+        if (command == "sqrt") return sqrt(a);
+        if (command == "sin") return sin(a);
+        if (command == "cos") return cos(a);
+        //if (command == "abs") return abs(a);
+        if (command == "exp") return exp(a);
+        if (command == "cbrt") return cbrt(a);
+        if (command == "tan") return tan(a);
+        if (command == "log") return log(a);
+        if (command == "log2") return log2(a);
+        if (command == "log10") return log10(a);
+        if (command == "arcsin") return asin(a);
+        if (command == "arccos") return acos(a);
+        if (command == "arctan") return atan(a);
 
-    	cout << "Compilition error! Illegal function " << command;
-    	_break_();
+        cout << "Compilition error! Illegal function " << command;
+        _break_();
 
-    	return 0;
+        return 0;
     }
 
     /// Formatting expretion
@@ -177,7 +177,7 @@ namespace Parse {
                 continue;
             }
             if (ch == ':') {
-            	ch = '/';
+                ch = '/';
             }
             if (ch == '(') brackets++;
             if (ch == ')') {
@@ -188,7 +188,7 @@ namespace Parse {
                 brackets--;
             }
             if (isLet(ch)) {
-            	ch = toLow(ch);
+                ch = toLow(ch);
             }
 
             if (prev_ch == -1) {
@@ -218,10 +218,10 @@ namespace Parse {
                 S += '*';
             }
             if (ch == '.') {
-            	if (prev_ch == ')') {
-            		S += '*';
-            	}
-            	S += '0';
+                if (prev_ch == ')') {
+                    S += '*';
+                }
+                S += '0';
             }
             S += ch;
             prev_ch = ch;
@@ -243,63 +243,63 @@ namespace Parse {
     }
 
     /// Main parsing
-    void main(const string& s, Stack*& res, Stack*& ops) {
-    	for (i = 0; i < s.length(); i++) {
+    void main(const string& s) {
+        for (i = 0; i < s.length(); i++) {
             char ch = s[i];
 
             if (isNum(ch)) {
 
-            	double num = 0;
-            	long long k = 10;
+                double num = 0;
+                long long k = 10;
 
-	            while (isNum(ch)) {
-	            	num *= 10;
-	            	num += toNum(ch);
-	            	ch = s[++i];
-	            }
+                while (isNum(ch)) {
+                    num *= 10;
+                    num += toNum(ch);
+                    ch = s[++i];
+                }
 
-	            if (ch == '.') {
-	            	ch = s[++i];
-	            	while (isNum(ch)) {
-	            		num += (double)toNum(ch) / k;
-	            		k *= 10;
-	            		ch = s[++i];
-	            	}
-	            }
+                if (ch == '.') {
+                    ch = s[++i];
+                    while (isNum(ch)) {
+                        num += (double)toNum(ch) / k;
+                        k *= 10;
+                        ch = s[++i];
+                    }
+                }
 
-	            push(res, num);
+                push(res, num);
 
-	            if (i >= s.length()) break;
-	        }
+                if (i >= s.length()) break;
+            }
 
-	        if (isLet(ch)) {
-	        	string command = "";
-	        	while (isLet(ch)) {
-	        		command += ch;
-	        		ch = s[++i];
-	        	}
+            if (isLet(ch)) {
+                string command = "";
+                while (isLet(ch)) {
+                    command += ch;
+                    ch = s[++i];
+                }
 
-	        	string exp = "";
-	        	if (ch == '(') {
-	        		exp += '(';
-	        		int brackets = 0;
-	        		ch = s[++i];
-	        		while (brackets >= 0) {
-	        			if (ch == '(') {
-	        				brackets++;
-	        			}
-	        			if (ch == ')') {
-	        				brackets--;
-	        			}
-	        			exp += ch;
-	        			ch = s[++i];
-	        		}
-	        	}
-	        	
-	        	push(res, doCommand(command, parse(exp, DEBUG_LOG)));
+                string exp = "";
+                if (ch == '(') {
+                    exp += '(';
+                    int brackets = 0;
+                    ch = s[++i];
+                    while (brackets >= 0) {
+                        if (ch == '(') {
+                            brackets++;
+                        }
+                        if (ch == ')') {
+                            brackets--;
+                        }
+                        exp += ch;
+                        ch = s[++i];
+                    }
+                }
 
-	        	if (i >= s.length()) break;
-	        }
+                push(res, doCommand(command, parse(exp, DEBUG_LOG)));
+
+                if (i >= s.length()) break;
+            }
 
             int priority_ch = priority(ch);
 
@@ -309,13 +309,13 @@ namespace Parse {
 
                 if (ch == ')') {
                     while ((*ops).field != '(') {
-                        push(res, doit(pop(ops), res, ops));
+                        push(res, doit(pop(ops)));
                     }
                     pop(ops);
 
                 } else {
                     while (!isEmpty(ops) && (*ops).field != '(' && priority_ch <= priority((*ops).field)) {
-                        push(res, doit(pop(ops), res, ops));
+                        push(res, doit(pop(ops)));
                     }
                     push(ops, ch);
                 }
@@ -325,31 +325,41 @@ namespace Parse {
         if (DEBUG_LOG) cout << "End reading line\n";
 
         while (ops != NULL) {
-            push(res, doit(pop(ops), res, ops));
+            push(res, doit(pop(ops)));
         }
 
         if (DEBUG_LOG) cout << "\n === End Debug log ===\n\n\n";
     }
-};
 
-using namespace Parse;
+public:
+    Parse(const string& _s, const bool& _DEBUG_LOG) {
+        s = _s;
+        DEBUG_LOG = _DEBUG_LOG;
 
-double parse(const string& _s, const bool& _DEBUG_LOG) {
-	Stack* res = NULL;
-	Stack* ops = NULL;
-
-    s = _s; DEBUG_LOG = _DEBUG_LOG;
-
-    s = formatting(s);
-
-    main(s, res, ops);
-
-    if ((*res).next != NULL) {
-        cout << "Something is wrong, sorry ((";
-        _break_();
+        s = formatting(s);
     }
 
-    return (*res).field;
+    double _parse_() {
+        res = NULL; ops = NULL;
+
+        main(s);
+
+        if ((*res).next != NULL) {
+            cout << "Something is wrong, sorry ((";
+            _break_();
+        }
+
+        return (*res).field;
+    }
+};
+
+double parse(const string& _s, const bool& _DEBUG_LOG) {
+    Parse P(_s, _DEBUG_LOG);
+    return P._parse_();
+}
+
+double parse(const string& _s) {
+    return parse(_s, 0);
 }
 
 #endif // PARSE_H_INCLUDED
