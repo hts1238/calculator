ab12ec7c3be39ded8f906a854e5964f4d1e7ea16
Timur <hts1238@mail.ru>
2019-09-20 22:56:55 +0500

    Variables were added

---
+++ qparse.h
@@ -0,0 +1,27 @@
+#ifndef QPARSE_H_INCLUDED
+#define QPARSE_H_INCLUDED
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include "parse.h"
+
+/// Решает квадратные уравнения
+vector<double> sq(const string& s, const char& ch) {
+    Parse P(s, 0);
+
+    map<char,double> m = {{ch, 0}};
+    l1 = P._parse_(m);
+
+    m[ch] = 1;
+    l2 = P._parse_(m);
+
+    m[ch] = 2;
+    l3 = P._parse_(m);
+
+    vector<double> res;
+    res.push_back(P._parse_(m));
+    return res;
+}
+
+#endif // QPARSE_H_INCLUDED
